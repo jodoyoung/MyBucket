38e933a8b5db9b8d62cb1317d18faad962c0d90d
jodoyoung <jodoyoung36@gmail.com>
2019-10-29 23:21:45 +0900

    init

---
+++ src/main/java/com/anajoa/grape/Grape.java
@@ -0,0 +1,8 @@
+package com.anajoa.grape;
+
+public class Grape {
+
+    public static void main(String[] args) {
+        System.out.println("hell");
+    }
+}
---
+++ src/test/java/com/anajoa/grape/GrapeTest.java
@@ -0,0 +1,4 @@
+package com.anajoa.grape;
+
+public class GrapeTest {
+}
