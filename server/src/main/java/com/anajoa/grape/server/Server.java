a69d5aaca20557f933773db30fe850e676c8db65
zero.jo <zero.jo@kakaocorp.com>
2019-11-01 08:21:38 +0900

    netty debug message ignore

--- server/src/main/java/com/anajoa/grape/server/Server.java
+++ server/src/main/java/com/anajoa/grape/server/Server.java
@@ -6,6 +6,10 @@ import reactor.netty.http.server.HttpServer;
 public class Server {
 
     public static void main(String[] args) {
+        // required add jvm options
+        // --add-opens java.base/jdk.internal.misc=io.netty.common --add-opens java.base/java.nio=io.netty.common
+        System.setProperty("io.netty.tryReflectionSetAccessible", "true");
+
         DisposableServer server =
                 HttpServer.create()
                         .port(80)
